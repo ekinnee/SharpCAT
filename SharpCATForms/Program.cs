0c4ff0d019ad36f1d490eb6961e71713b5d693f7
Erick Kinnee <erick.kinnee@fairwaymc.com>
2019-03-06 10:19:57 -0600

    Re-org because I forgot to include the test forms app.

---
+++ SharpCATForms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpCATForms
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            //SharpCAT sharpCAT = new SharpCAT();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
